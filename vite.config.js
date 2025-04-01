0f94161347174a6d0981baa3769d7b86af2f8a1b
Hanhee Lee <hanhee@hotmail.ca>
2025-04-01 00:07:02 -0400

    clean slate

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/', // Change this to '/lee-hanhee.github.io/' for GitHub Pages production build
-}) 
